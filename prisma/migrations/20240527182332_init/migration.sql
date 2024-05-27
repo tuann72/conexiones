ccad16c423b8f1881f5f815261aae00be157ae65
Tuan Nguyen <tuannguyen08426@gmail.com>
2024-05-27 13:24:22 -0500

    init prisma migrations

---
+++ prisma/migrations/20240527182332_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Combination" (
+    "id" INTEGER NOT NULL,
+    "word1" TEXT NOT NULL,
+    "word2" TEXT NOT NULL,
+    "word3" TEXT NOT NULL,
+    "word4" TEXT NOT NULL,
+
+    CONSTRAINT "Combination_pkey" PRIMARY KEY ("id")
+);
